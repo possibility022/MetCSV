abb8799c1077722e7ff357448ded736e24b26884
Bartek <Bartek@KOMP7>
2017-04-18 15:38:03 +0200

    Zmieniono Encoding na UTF-8 Przy eksportowanych dokumentach. Dodano metodę "Plik otwarty w innym programie" ale nigdzie jej jeszcze nie używam. Dodano również obsługę błędu jeśli plik METowy jest otwarty a program próbuję pobrać nowy.

--- METCSV/Network/Met.cs
+++ METCSV/Network/Met.cs
@@ -25,8 +25,17 @@ namespace METCSV.Network
         {
             using (var client = new WebClient())
             {
-                client.DownloadFile("http://met.redcart.pl/export/d9b11de494035a84e68e5faa6063692a.csv", fileName);
+                try
+                {
+                    client.DownloadFile("http://met.redcart.pl/export/d9b11de494035a84e68e5faa6063692a.csv", fileName);
+                } catch (System.Net.WebException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show("Problem z plikiem: " + fileName + ". Sprawdz czy nie jest on otwarty w jakims programie");
+                    throw ex;
+                }
             }
+
+
             done();
         }
     }
