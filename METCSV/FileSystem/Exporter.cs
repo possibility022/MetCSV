c9542327ee93b6586cb69ee9e0d0426c9e846aa7
Possibility <tomaszbakk@outlook.com>
2018-01-19 22:55:30 +0100

    Importing wpf project

--- METCSV/FileSystem/Exporter.cs
+++ METCSV/FileSystem/Exporter.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace METCSV.FileSystem
