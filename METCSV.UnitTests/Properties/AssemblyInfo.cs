4de2cf792c31d7522620df693df68d78cfc2a4d8
Possibility <tomaszbakk@outlook.com>
2018-01-20 21:54:40 +0100

    Dodano test wydajnościowy. Hashset vs List (Linq i .Contains)

---
+++ METCSV.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("METCSV.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("METCSV.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9082dfde-1c28-481a-8464-e5eeb0decc4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ METCSV.WPF/Mergers/ProductMerger.cs
@@ -0,0 +1,57 @@
+﻿using METCSV.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METCSV.WPF.Mergers
+{
+    class ProductMerger
+    {
+        IEnumerable<Product> _metProducts;
+        HashSet<string> _hiddenProducts;
+
+        public void SetMetProducts(IEnumerable<Product> products)
+        {
+            _metProducts = products;
+        }
+
+        private void removeHiddenProducts(List<Product> products)
+        {
+            //LOG("Usuwam ukryte produkty"); //todo log this
+            int countAtBegining = products.Count;
+
+            CreateHiddenProductsCollection();
+            
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (_hiddenProducts.Contains(products[i].SymbolSAP))
+                {
+                    products.RemoveAt(i);
+                    i--;
+                }
+            }
+            
+            //log Database.Log.log("Usunięto " + (countAtBegining - products.Count).ToString()); //todo log or move it to return
+        }
+
+        private void CreateHiddenProductsCollection()
+        {
+            if (_hiddenProducts == null)
+            {
+                HashSet<string> hiddenProducts = new HashSet<string>();
+
+                foreach (var prod in _metProducts)
+                {
+                    if (prod.Hidden)
+                    {
+                        hiddenProducts.Add(prod.SymbolSAP);
+                    }
+                }
+
+                _hiddenProducts = hiddenProducts;
+            }
+        }
+    }
+}
