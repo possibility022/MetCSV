d7c0c76efedd90b3ccead6ccce86fde2d5b517fe
Tomasz Bąk <InfGremlin022@hotmail.com>
2018-02-06 20:50:30 +0100

    Creating UnitTests for Profits

--- METCSV.WPF/Models/Profits.cs
+++ METCSV.WPF/Models/Profits.cs
@@ -12,15 +12,13 @@ namespace METCSV.WPF.Models
 {
     public class Profits : BindableBase
     {
-        private double _defaultProfit;
+        private double _defaultProfit = 0.1;
         private HashSet<string> _allProviders;
         private Dictionary<string, double> _values;
 
         public string Provider { get; }
 
         public IReadOnlyDictionary<string, double> Values { get => _values; }
-
-        public HashSet<string> AllProviders { get => _allProviders; set => SetProperty(ref _allProviders, value); }
 
         public double DefaultProfit { get => _defaultProfit; set => SetProperty(ref _defaultProfit, value); }
 
@@ -30,15 +28,44 @@ namespace METCSV.WPF.Models
             RaisePropertyChanged(nameof(Provider));
         }
 
+        /// <summary>
+        /// Sets the new profits.
+        /// </summary>
+        /// <param name="newValues">The new values.</param>
         public void SetNewProfits(IEnumerable<EditableDictionaryKey<string, double>> newValues)
         {
-            Dictionary<string, double> values = new Dictionary<string, double>();
-            foreach (var val in newValues)
-            {
-                values.Add(val.Key, val.Value);
-            }
+            if (_values == null)
+                _values = new Dictionary<string, double>();
 
-            _values = values;
+            foreach(var newValue in newValues)
+            {
+                if (_values.ContainsKey(newValue.Key) == false)
+                {
+                    _values.Add(newValue.Key, newValue.Value);
+                }
+                else
+                {
+                    _values[newValue.Key] = newValue.Value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the manufacturer with default values if does not exists in collection.
+        /// </summary>
+        /// <param name="manufacturers">The manufacturers.</param>
+        public void AddManufacturers(IEnumerable<string> manufacturers)
+        {
+            if (_values == null)
+                _values = new Dictionary<string, double>();
+
+            foreach(var manu in manufacturers)
+            {
+                if (_values.ContainsKey(manu) == false)
+                {
+                    _values.Add(manu, DefaultProfit);
+                }
+            }
         }
     }
 }
