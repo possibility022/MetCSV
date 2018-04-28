f633c7856a85bdf67485ba08375cf39d82deaa1a
Tomasz Bąk <tomaszbakk@outlook.com>
2018-04-28 16:33:29 +0200

    End Of Live domain.

---
+++ METCSV.UnitTests/EngineTest/EndOfLiveDomainTest.cs
@@ -0,0 +1,87 @@
+﻿using METCSV.Common;
+using METCSV.WPF.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace METCSV.UnitTests.EngineTest
+{
+    [TestClass]
+    public class EndOfLiveDomainTest
+    {
+
+        Product[] _shortMetList;
+        Product[] _shortProviderList;
+
+        [TestInitialize]
+        public void InitializeData()
+        {
+            //Arrange
+            _shortMetList = new[] {
+                new Product {SymbolSAP = "ABC1", NazwaProducenta = "Producent1", OryginalnyKodProducenta = "A"},
+                new Product {SymbolSAP = "ABC2", NazwaProducenta = "Producent2", OryginalnyKodProducenta = "B"},
+                new Product {SymbolSAP = "ABC3", NazwaProducenta = "Producent3", OryginalnyKodProducenta = "C"}
+            };
+            
+            _shortProviderList = new[] {
+                new Product() { SymbolSAP = "ABC", NazwaProducenta = "Producent", OryginalnyKodProducenta = "A" },
+                new Product() { SymbolSAP = "ABC2", NazwaProducenta = "Producent2", OryginalnyKodProducenta = "_" }
+            };
+        }
+
+        [TestMethod]
+        public void DomainWillNotRemoveProductsFromCurrentCollection()
+        {
+            // Arrange
+            var met = Factory.GetMetProducts();
+            var provider = Factory.GetABProducts();
+            var metCount = met.Count;
+            
+            // Act
+            EndOfLiveDomain domain = new EndOfLiveDomain(met, provider);
+
+            // Assert
+            Assert.AreEqual(metCount, met.Count);
+        }
+
+        [TestMethod]
+        public void IfProvideListContainsSapMenuHashDoNotSetEOL()
+        {
+            // Arrange
+            var productWithOutEol = _shortMetList[1];
+            var domain = new EndOfLiveDomain(_shortMetList, _shortProviderList);
+
+            // Act
+            domain.SetEndOfLife();
+
+            // Assert
+            Assert.AreNotEqual("EOL", productWithOutEol.Kategoria);
+        }
+
+        [TestMethod]
+        public void IfProvideListContainsManufacturerCodeDoNotSetEOL()
+        {
+            // Arrange
+            var productWithOutEol = _shortMetList[0];
+            var domain = new EndOfLiveDomain(_shortMetList, _shortProviderList);
+
+            // Act
+            domain.SetEndOfLife();
+
+            // Assert
+            Assert.AreNotEqual("EOL", productWithOutEol.Kategoria);
+        }
+
+        [TestMethod]
+        public void IfThereIsNoSapManuHashOrManufacturerCodeToMatchSetEOL()
+        {
+            // Arrange
+            var productWithEol = _shortMetList[2];
+            var domain = new EndOfLiveDomain(_shortMetList, _shortProviderList);
+
+            // Act
+            domain.SetEndOfLife();
+
+            // Assert
+            Assert.AreEqual("EOL", productWithEol.Kategoria);
+        }
+    }
+}
