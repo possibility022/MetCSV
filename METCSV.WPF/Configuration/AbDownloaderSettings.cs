7db327d16662993d2fdaccf3da998a6a67f4e5f0
Tomasz Bąk <tomaszbakk@outlook.com>
2018-06-14 23:16:08 +0200

    Settings for AB

---
+++ METCSV.WPF/Configuration/AbDownloaderSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace METCSV.WPF.Configuration
+{
+    public class AbDownloaderSettings
+    {
+
+        public string ZippedFile { get; set; } = "ab.zip";
+
+        public string FolderToExtract { get; set; } = "ExtractedFiles_AB";
+
+        public string EmailServerAddress { get; set; } = "mail.met.com.pl";
+
+        public int EmailServerPort { get; set; } = 110;
+
+        public bool EmailServerUseSSL { get; set; } = false;
+
+        public string EmailLogin { get; set; }
+
+        public string EmailPassword { get; set; }
+
+        public bool DeleteOldValues { get; set; } = true;
+    }
+}
