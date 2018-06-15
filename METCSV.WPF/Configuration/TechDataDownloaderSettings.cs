9c0caa88bb7320e915b33722c05f753ee73a4830
Possibility <tomaszbakk@outlook.com>
2018-06-15 19:19:10 +0200

    TechData settings

---
+++ METCSV.WPF/Configuration/TechDataDownloaderSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METCSV.WPF.Configuration
+{
+    class TechDataDownloaderSettings
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string FtpAddress { get; set; } = "ftp2.techdata-it-emea.com";
+
+        public string Pattern { get; set; } = "(20[1-5][0-9])-((0[0-9])|(1[0-2]))-(([0-2][0-9])|(3[0-1]))";
+
+        public string FolderToExtract { get; set; } = "ExtractedFiles";
+
+        public string CsvMaterials { get; set; } = "TD_Material.csv";
+
+        public string CsvPrices { get; set; } = "TD_Prices.csv";
+    }
+}
