9ce19590ebdf2ea416ef1b3c3388b82cd47f37b7
Possibility <tomaszbakk@outlook.com>
2018-06-15 19:07:46 +0200

    Met downlaoder settings

---
+++ METCSV.WPF/Configuration/MetDownloaderSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METCSV.WPF.Configuration
+{
+    public class MetDownloaderSettings
+    {
+        public string CsvFile { get; set; } = "met.csv";
+
+        public string Url { get; set; } = "http://met.redcart.pl/export/d9b11de494035a84e68e5faa6063692a.csv";
+    }
+}
--- METCSV.WPF/Downloaders/MetDownloader.cs
+++ METCSV.WPF/Downloaders/MetDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Threading;
 using METCSV.Common;
+using METCSV.WPF.Configuration;
 using METCSV.WPF.Enums;
 
 namespace METCSV.WPF.Downloaders
@@ -9,7 +10,7 @@ namespace METCSV.WPF.Downloaders
     {
         public override Providers Provider => Providers.MET;
 
-        private string _fileName = "met.csv"; //todo move to config
+        private string _fileName = Settings.MetDownlaoder.CsvFile;
 
         public MetDownloader(CancellationToken token)
         {
@@ -23,10 +24,12 @@ namespace METCSV.WPF.Downloaders
 
             using (var client = new WebClient())
             {
-                client.DownloadFile("http://met.redcart.pl/export/d9b11de494035a84e68e5faa6063692a.csv", _fileName); //Stary link 
+                client.DownloadFile(Settings.MetDownlaoder.Url, _fileName);
 
                 //todo implement cancelation token
-
+                
+            
+                // "http://met.redcart.pl/export/d9b11de494035a84e68e5faa6063692a.csv" // Stary link ale jest uzwyany?
                 //client.DownloadFile("http://met.redcart.pl/export/9900a7cdd99448e6d1080827e09c73da.csv", fileName); //Nowy link
             }
 
