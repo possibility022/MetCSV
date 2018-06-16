a90ce96a561e6ec7d33c65e87c19653cb9a39d09
Tomasz Bąk <tomaszbakk@outlook.com>
2018-06-16 21:19:55 +0200

    During implementing background of settings.

---
+++ METCSV.WPF/Helpers/PropertyCopy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METCSV.WPF.Helpers
+{
+    static class PropertyCopy
+    {
+
+        public static bool AnyChanges<T>(T source, T target)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
