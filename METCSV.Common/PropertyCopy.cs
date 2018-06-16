32505b22c6e3b5dc439f60ec40a2b1fe585304f8
Tomasz Bąk <tomaszbakk@outlook.com>
2018-06-16 22:21:53 +0200

    PropertyCopy Any Changes implementation.

---
+++ METCSV.Common/PropertyCopy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METCSV.Common
+{
+    public static class PropertyCopy
+    {
+
+        public static bool AnyChanges<T>(T source, T target) where T : class
+        {
+            if (source == target)
+                return false;
+            
+            var properties = source.GetType().GetProperties();
+
+            foreach(var prop in properties)
+            {
+                var targetVal = prop.GetValue(target);
+                var sourceVal = prop.GetValue(source);
+
+                if (!sourceVal.Equals(targetVal))
+                {
+                    return true;
+                }
+            }
+
+             return false;
+        }
+
+    }
+}
---
+++ METCSV.UnitTests/PropertyCopyTest.cs
@@ -0,0 +1,58 @@
+﻿using METCSV.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace METCSV.UnitTests
+{
+
+    class ObjectToCopy
+    {
+        public string Str { get; set; }
+        public int Integer { get; set; }
+
+        public bool Boolean { get; set; }
+    }
+
+    [TestClass]
+    public class PropertyCopyTest
+    {
+
+        ObjectToCopy A = new ObjectToCopy { Str = "ABC", Boolean = false, Integer = 55 };
+        ObjectToCopy B = new ObjectToCopy { Str = "ABC", Boolean = false, Integer = 55 };
+        ObjectToCopy C = new ObjectToCopy { Str = "321", Boolean = false, Integer = 55 };
+        ObjectToCopy D = new ObjectToCopy { Str = "ABC", Boolean = true, Integer = 55 };
+        ObjectToCopy E = new ObjectToCopy { Str = "ABC", Boolean = false, Integer = 10 };
+
+
+        [TestMethod]
+        public void AllAreEqual()
+        {
+            var result = PropertyCopy.AnyChanges(A, B);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void StrIsDifferent()
+        {
+            var result = PropertyCopy.AnyChanges(A, C);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void BooleanIsDifferent()
+        {
+            var result = PropertyCopy.AnyChanges(A, D);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntIsDifferent()
+        {
+            var result = PropertyCopy.AnyChanges(A, E);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
