d1613faec560ff99ac9fec5a44f29708dd3cc34d
Possibility <tomaszbakk@outlook.com>
2018-06-16 21:19:55 +0200

    During implementing background of settings.

--- METCSV.WPF/ViewModels/SettingsViewModel.cs
+++ METCSV.WPF/ViewModels/SettingsViewModel.cs
@@ -1,14 +1,38 @@
 ﻿using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace METCSV.WPF.ViewModels
 {
     class SettingsViewModel : BindableBase
     {
 
+        private bool _metTabIsActive;
+        public bool MetTabIsActive
+        {
+            get { return _metTabIsActive; }
+            set { SetProperty(ref _metTabIsActive, value); }
+        }
+
+        private bool _abTabIsActive;
+        public bool AbTabIsActive
+        {
+            get { return _abTabIsActive; }
+            set { SetProperty(ref _abTabIsActive, value); }
+        }
+
+        private bool _tdTabIsActive;
+        public bool TdTabIsActive
+        {
+            get { return _tdTabIsActive; }
+            set { SetProperty(ref _tdTabIsActive, value); }
+        }
+
+        private bool _lamaTabIsActive;
+        public bool LamaTabIsActive
+        {
+            get { return _lamaTabIsActive; }
+            set { SetProperty(ref _lamaTabIsActive, value); }
+        }
+
     }
+
 }
