68c9cfe7ee412adddf1c1f103967e4b960ffbc45
Possibility <tomaszbakk@outlook.com>
2018-06-16 22:04:49 +0200

    Ab settings backend.

---
+++ METCSV.WPF/Converters/StringToIntConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace METCSV.WPF.Converters
+{
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parsed = int.TryParse((string)value, out int v);
+            if (parsed)
+            {
+                return v;
+            }
+            else
+            {
+                return new object(); // what we should return in this case?
+            }
+        }
+    }
+}
