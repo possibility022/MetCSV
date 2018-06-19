3dc631c78a24ce6747bc9006b97abefec2d0d143
Tomasz Bąk <tomaszbakk@outlook.com>
2018-06-19 20:26:55 +0200

    Removing hardcoded values.

--- METCSV.WPF/Configuration/LamaDownloaderSettings.cs
+++ METCSV.WPF/Configuration/LamaDownloaderSettings.cs
@@ -8,9 +8,9 @@
 
         public string Url { get; set; } = "http://www.lamaplus.com.pl/partner/export.php";
 
-        public string Login { get; set; } = "60117701";
+        public string Login { get; set; } = string.Empty;
 
-        public string Password { get; set; } = "10d94d324aeecb637000dd70682c0517";
+        public string Password { get; set; } = string.Empty;
 
         public string Request { get; set; } = "priceList";
     }
