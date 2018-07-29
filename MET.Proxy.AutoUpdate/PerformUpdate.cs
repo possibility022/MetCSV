1c59886b8fc5123f81b01d932fbdde2693ac57f5
Tomasz Bąk <tomaszbakk@outlook.com>
2018-07-29 21:21:32 +0200

    Initial settings for autoupdate

---
+++ MET.Proxy.AutoUpdate/PerformUpdate.cs
@@ -0,0 +1,12 @@
+﻿using AutoUpdaterDotNET;
+
+namespace MET.Proxy.AutoUpdate
+{
+    public static class PerformUpdate
+    {
+        public static void Update()
+        {
+            AutoUpdater.Start("http://192.168.0.6/metcsv/version.xml");
+        }
+    }
+}
