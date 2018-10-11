3743bdd88ee2a96459cb0e1f5e9170836a5788a2
Possibility <tomaszbakk@outlook.com>
2018-10-12 01:10:16 +0200

    Logging hidden products.

--- METCSV.Common/Formatters/IObjectFormatter.cs
+++ METCSV.Common/Formatters/IObjectFormatter.cs
@@ -5,8 +5,11 @@ namespace METCSV.Common.Formatters
 {
     public interface IObjectFormatter<T>
     {
-        void Get(StringBuilder sb, IEnumerable<T> item);
+        void Get(StringBuilder sb, IEnumerable<T> items);
 
         void Get(StringBuilder sb, T item);
+
+        string Get(T item);
+        string Get(IEnumerable<T> items);
     }
 }
