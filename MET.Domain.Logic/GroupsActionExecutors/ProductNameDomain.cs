8e2eadff2a96123c8ba0ea9434243de47e6f5ed6
Possibility <tomaszbakk@outlook.com>
2021-08-09 00:33:50 +0200

    Product Name

---
+++ MET.Domain.Logic/GroupsActionExecutors/ProductNameDomain.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using METCSV.Common.Formatters;
+
+namespace MET.Domain.Logic
+{
+    public class ProductNameDomain : IActionExecutor
+    {
+
+        /// 1. Nazwa produktu tak jak w pliku CSV ze sklepu met.
+        /// 2. Jeżeli produkt jest nowy to wtedy nazwa jak z pliku od dystrybutora. Priorytety:
+        /// Lama
+        /// TD
+        /// AB
+
+        public void ExecuteAction(string partNumber, ICollection<Product> vendorProducts, ICollection<Product> metProducts, IObjectFormatter<object> objectFormatter)
+        {
+            objectFormatter.WriteLine("Ustawiam nazwe produktu dla " + partNumber);
+
+            string name;
+
+            if (!vendorProducts.Any())
+                return;
+
+            if (metProducts.Any())
+            {
+                objectFormatter.WriteLine("Mamy nazwe produktu ze sklepu met.");
+
+                if (metProducts.Count > 1)
+                {
+                    objectFormatter.WriteLine("Mamy więcej niż jeden produkt dla tego part numberu na liście CSV z MET. Używam pierwszego na liście");
+                }
+
+                name = metProducts.First().NazwaProduktu;
+            }
+            else
+            {
+                name = SelectName(vendorProducts);
+            }
+
+            foreach(var prod in vendorProducts)
+            {
+                prod.NazwaProduktu = name;
+            }
+        }
+
+        private static IReadOnlyDictionary<Providers, int> Priorieties = new Dictionary<Providers, int>
+        {
+            {Providers.Lama, 0 },
+            {Providers.TechData, 1 },
+            {Providers.AB, 2 },
+        };
+
+        private string SelectName(ICollection<Product> vendorProducts)
+        {
+            var first = vendorProducts.First();
+            if (first != null)
+            {
+                foreach (var prod in vendorProducts)
+                {
+                    if (ReferenceEquals(first, prod))
+                        continue;
+
+                    if (Priorieties[prod.Provider] < Priorieties[first.Provider])
+                    {
+                        first = prod;
+                    }
+                }
+            }
+
+            return first.NazwaProduktu;
+        }
+    }
+}
---
+++ METCSV.UnitTests/EngineTest/ProductNameDomainTests.cs
@@ -0,0 +1,78 @@
+﻿using MET.Domain;
+using MET.Domain.Logic;
+using METCSV.Common.Formatters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace METCSV.UnitTests.EngineTest
+{
+    [TestClass]
+    public class ProductNameDomainTests
+    {
+        ProductNameDomain executor;
+
+        Product[] list;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            executor = new ProductNameDomain();
+            list = new[] {
+                new Product (Providers.Lama){ UrlZdjecia = string.Empty, SymbolSAP = "ABC", NazwaProducenta = "Producent", ID = 1, NazwaProduktu = "Nazwa1" },
+                new Product (Providers.AB){ UrlZdjecia = string.Empty, SymbolSAP = "ABC", NazwaProducenta = "Producent", ID = 2, NazwaProduktu = "Nazwa2"   },
+                new Product (Providers.TechData){ UrlZdjecia = "SomeURL", SymbolSAP = "ABC", NazwaProducenta = "Producent", ID = 3, NazwaProduktu = "Nazwa3" } };
+        }
+
+        [TestMethod]
+        public void PrioritiesTest_LamaIs1st()
+        {
+            executor.ExecuteAction("XYZ",
+                list,
+            new Product[] { },
+            new ZeroOutputFormatter());
+
+            Assert.IsTrue(list.All(r => r.NazwaProduktu == "Nazwa1"));
+        }
+
+        [TestMethod]
+        public void PrioritiesTest_TechDataIs2nd()
+        {
+            list = list.Where(r => r.Provider != Providers.Lama).ToArray();
+
+            executor.ExecuteAction("XYZ",
+                list,
+                new Product[] { },
+                new ZeroOutputFormatter());
+
+            Assert.IsTrue(list.All(r => r.NazwaProduktu == "Nazwa3"));
+        }
+
+
+        [TestMethod]
+        public void PrioritiesTest_TechDataIs3rd()
+        {
+            list = list.Where(r => r.Provider != Providers.Lama && r.Provider != Providers.TechData).ToArray();
+
+            executor.ExecuteAction("XYZ",
+                list,
+                new Product[] { },
+                new ZeroOutputFormatter());
+
+            Assert.IsTrue(list.All(r => r.NazwaProduktu == "Nazwa2"));
+        }
+
+        [TestMethod]
+        public void PrioritiesTest_MetTopHasPriority()
+        {
+            executor.ExecuteAction("XYZ",
+                list,
+                new Product[]
+                {
+                    new Product (Providers.MET){ UrlZdjecia = "SomeURL", SymbolSAP = "ABC", NazwaProducenta = "Producent", ID = 3, NazwaProduktu = "Nazwa4" }
+                },
+                new ZeroOutputFormatter());
+
+            Assert.IsTrue(list.All(r => r.NazwaProduktu == "Nazwa4"));
+        }
+    }
+}
