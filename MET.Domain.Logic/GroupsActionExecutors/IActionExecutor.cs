d59ee6251a9a99e0eb801a9ab4b2e51bb2d9ac70
Possibility <tomaszbakk@outlook.com>
2021-08-09 00:33:50 +0200

    Product Name

---
+++ MET.Domain.Logic/GroupsActionExecutors/IActionExecutor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using METCSV.Common.Formatters;
+
+namespace MET.Domain.Logic
+{
+    public interface IActionExecutor
+    {
+        void ExecuteAction(string partNumber, ICollection<Product> vendorProducts, ICollection<Product> metProducts, IObjectFormatter<object> objectFormatter);
+    }
+}
