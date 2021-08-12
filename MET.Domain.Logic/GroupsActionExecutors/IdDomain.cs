c62944f98dbe2ffa5accd090fd8e07d51bd7026a
Possibility <tomaszbakk@outlook.com>
2021-08-12 23:33:08 +0200

    IdDomain

---
+++ MET.Domain.Logic/GroupsActionExecutors/IdDomain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using MET.Domain.Logic.Models;
+
+namespace MET.Domain.Logic.GroupsActionExecutors
+{
+    public class IdDomain : IActionExecutor
+    {
+        public void ExecuteAction(ProductGroup productGroup)
+        {
+            if (!productGroup.MetProducts.Any())
+            {
+                productGroup.FinalProduct.ID = null;
+            }
+            else
+            {
+                if (productGroup.MetProducts.Select(r => r.ID).Distinct().Count() > 1)
+                    throw new InvalidOperationException(
+                        "Wygląda na to, że w pliku met jest wiele produktów dla tej samej grupy. Nie wiem którego Id użyć.");
+                var idToSet = productGroup.MetProducts.First().ID;
+
+                productGroup.FinalProduct.ID = idToSet;
+            }
+        }
+    }
+}
---
+++ METCSV.UnitTests/EngineTest/IdDomainTests.cs
@@ -0,0 +1,44 @@
+﻿using MET.Domain;
+using MET.Domain.Logic.Extensions;
+using MET.Domain.Logic.GroupsActionExecutors;
+using MET.Domain.Logic.Models;
+using METCSV.Common.Formatters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace METCSV.UnitTests.EngineTest
+{
+    [TestClass]
+    public class IdDomainTests
+    {
+        private IdDomain idDomain;
+        private ProductGroup productGroup;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            idDomain = new IdDomain();
+            productGroup = new ProductGroup(string.Empty, ZeroOutputFormatter.Instance);
+        }
+
+        [TestMethod]
+        public void IdIsTakenFromMetProduct()
+        {
+            productGroup.AddMetProduct(new Product(Providers.MET) { ID = 123, SymbolSAP = "ABC", NazwaProduktu = "Produkt", NazwaProducenta = "Producent", OryginalnyKodProducenta = "A" });
+
+            idDomain.ExecuteAction(productGroup);
+
+            Assert.AreEqual(123, productGroup.FinalProduct.ID);
+        }
+
+        [TestMethod]
+        public void IdIsNullWhenProductIsNew()
+        {
+            productGroup.AddVendorProducts(Factory.GetShortVendorList());
+
+            idDomain.ExecuteAction(productGroup);
+
+            Assert.AreEqual(null, productGroup.FinalProduct.ID);
+        }
+
+    }
+}
