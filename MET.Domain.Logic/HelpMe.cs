e1e77bd6ce2a1678d0d14d1e014661c0bf84048b
Possibility <tomaszbakk@outlook.com>
2021-08-15 22:41:46 +0200

    SAP Number

---
+++ MET.Domain.Logic/HelpMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MET.Domain.Logic
+{
+    static class HelpMe
+    {
+        public static void ThrowIfNull(Product product)
+        {
+            if (product == null)
+                throw new InvalidOperationException(
+                    "Product is null.");
+        }
+    }
+}
