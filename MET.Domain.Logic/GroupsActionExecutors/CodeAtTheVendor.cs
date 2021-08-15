323cd18b13995d70cf134ac24dcf0d08255af549
Possibility <tomaszbakk@outlook.com>
2021-08-15 22:26:13 +0200

    New Interface to execute action based on source product

---
+++ MET.Domain.Logic/GroupsActionExecutors/CodeAtTheVendor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MET.Domain.Logic.GroupsActionExecutors
+{
+    public class CodeAtTheVendor: IFinalProductConstructor
+    {
+        private string GetCode(Product product)
+        {
+            return $"{product.Provider}_{product.KodDostawcy}";
+        }
+
+        public void ExecuteAction(Product source, Product final)
+        {
+            if (source == null)
+                throw new InvalidOperationException(
+                    "Can not set 'Code at the vendor' as the cheapest product is null.");
+
+            final.KodDostawcy = GetCode(source);
+        }
+    }
+}
---
+++ MET.Domain.Logic/GroupsActionExecutors/IFinalProductConstructor.cs
@@ -0,0 +1,7 @@
+﻿namespace MET.Domain.Logic.GroupsActionExecutors
+{
+    public interface IFinalProductConstructor
+    {
+        void ExecuteAction(Product source, Product final);
+    }
+}
