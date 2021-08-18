7d55f539d5bed45b0305e37ecc7e65de624876e8
Possibility <tomaszbakk@outlook.com>
2021-08-18 22:56:21 +0200

    Id domain update

--- MET.Domain.Logic/GroupsActionExecutors/IdDomain.cs
+++ MET.Domain.Logic/GroupsActionExecutors/IdDomain.cs
@@ -6,6 +6,13 @@ namespace MET.Domain.Logic.GroupsActionExecutors
 {
     public class IdDomain : IActionExecutor
     {
+        private readonly bool ignoreDuplicates;
+
+        public IdDomain(bool ignoreDuplicates)
+        {
+            this.ignoreDuplicates = ignoreDuplicates;
+        }
+
         public void ExecuteAction(ProductGroup productGroup)
         {
             if (!productGroup.MetProducts.Any())
@@ -15,8 +22,18 @@ namespace MET.Domain.Logic.GroupsActionExecutors
             else
             {
                 if (productGroup.MetProducts.Select(r => r.ID).Distinct().Count() > 1)
+                {
+                    if (ignoreDuplicates)
+                    {
+                        productGroup.ObjectFormatter.WriteLine(
+                            "Mamy więcej niż jedno Id dla tego produktu. Flaga jest ustawiona na ignorowanie takich przypadków. Id będzie nullem.");
+                        return;
+                    }
+
                     throw new InvalidOperationException(
                         "Wygląda na to, że w pliku met jest wiele produktów dla tej samej grupy. Nie wiem którego Id użyć.");
+                }
+
                 var idToSet = productGroup.MetProducts.First().ID;
 
                 productGroup.FinalProduct.ID = idToSet;
