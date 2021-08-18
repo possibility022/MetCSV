5be8a6401a0a63bc0e127ec2da5e01f3b13832c2
Possibility <tomaszbakk@outlook.com>
2021-08-18 22:57:21 +0200

    Source product selector domain

---
+++ MET.Domain.Logic/GroupsActionExecutors/SourceProductSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using MET.Domain.Logic.Models;
+
+namespace MET.Domain.Logic.GroupsActionExecutors
+{
+    public class SourceProductSelector : IActionExecutor
+    {
+        public void ExecuteAction(ProductGroup productGroup)
+        {
+            if (productGroup.FinalProduct.Kategoria == EndOfLiveDomain.EndOfLifeCategory)
+            {
+                productGroup.DataSourceProduct = productGroup.MetProducts.First();
+            }
+            else
+            {
+                productGroup.DataSourceProduct = productGroup.CheapestProduct;
+            }
+        }
+    }
+}
