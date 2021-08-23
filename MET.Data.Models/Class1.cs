fd00b514eb063d78b5379dc29d9d38de2d3efef1
Possibility <tomaszbakk@outlook.com>
2021-08-23 19:41:36 +0200

    New Projects

---
+++ MET.Data.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MET.Data.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ MET.Data.Storage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MET.Data.Storage
+{
+    public class Class1
+    {
+    }
+}
