90139642ceb6f3585502a43f1976bbad14f629de
Possibility <tomaszbakk@outlook.com>
2021-09-01 02:22:17 +0200

    Small improvement

--- METCSV.WPF/Views/MainWindow.xaml.cs
+++ METCSV.WPF/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using METCSV.WPF.ViewModels;
+﻿using System.Threading.Tasks;
+using METCSV.WPF.ViewModels;
 using METCSV.WPF.Views;
 using Microsoft.Win32;
 using System.Windows;
@@ -15,6 +16,8 @@ namespace METCSV.WPF
 
         MainWindowViewModel MainWindowViewModel { get => _mainWindowViewModel; }
 
+        private Task task;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,9 +26,14 @@ namespace METCSV.WPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            var task = MainWindowViewModel.StartClickAsync();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            if (task?.IsCompleted == true)
+            {
+                task = MainWindowViewModel.StartClickAsync();
+            }
+            else
+            {
+                MessageBox.Show("Zaczekaj, jeszcze generuje.");
+            }
         }
 
         private void ShowProfitsWindow(object sender, RoutedEventArgs e)
