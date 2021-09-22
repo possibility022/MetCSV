a59a93c479d8bef3102520b90da35f020883a0f8
Possibility <tomaszbakk@outlook.com>
2021-09-22 22:24:24 +0200

    Default profit is in settings

--- METCSV.WPF/Configuration/EngineSettings.cs
+++ METCSV.WPF/Configuration/EngineSettings.cs
@@ -4,17 +4,12 @@ namespace METCSV.WPF.Configuration
 {
     public class EngineSettings
     {
-        private bool _offlineMode = true;
-
-        public bool OfflineMode
-        {
-            get { return _offlineMode; }
-            set { _offlineMode = value; }
-        }
+        public bool OfflineMode { get; set; } = true;
 
         public bool SetProfits { get; set; } = true;
         public string NewVersionURL { get; set; } = "http://tbsys.ddns.net/metcsv/version.xml";
 
         public int MaximumPriceErrorDifference { get; set; } = 20;
+        public double DefaultProfit { get; set; } = 0.1;
     }
 }
