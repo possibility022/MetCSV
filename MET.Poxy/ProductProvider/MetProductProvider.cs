e2e71d9478684524a2595bf2c02f92a843c04dfe
Possibility <tomaszbakk@outlook.com>
2021-09-28 23:31:59 +0200

    Met Cleanup

--- MET.Poxy/ProductProvider/MetProductProvider.cs
+++ MET.Poxy/ProductProvider/MetProductProvider.cs
@@ -1,21 +1,23 @@
-﻿using MET.Domain;
-using MET.Proxy;
-using MET.Proxy.ProductReaders;
-using System.Threading;
+﻿using System.Threading;
 using MET.Data.Models;
+using MET.Proxy.Configuration;
 using MET.Proxy.Downloaders;
 using MET.Proxy.Downloaders.Offline;
 using MET.Proxy.Interfaces;
-using MET.Proxy.ProductProvider;
+using MET.Proxy.ProductReaders;
 
-namespace METCSV.WPF.ProductProvider
+namespace MET.Proxy.ProductProvider
 {
     class MetProductProvider : ProductProviderBase
     {
+        private readonly IMetSettings settings;
+        private readonly bool offlineMode;
         protected override string ArchiveFileNamePrefix => "MET";
 
-        public MetProductProvider(CancellationToken token) : base(token)
+        public MetProductProvider(IMetSettings settings, bool offlineMode ,CancellationToken token) : base(token)
         {
+            this.settings = settings;
+            this.offlineMode = offlineMode;
             SetProductDownloader(GetDownloader());
             SetProductReader(GetProductReader());
             Provider = Providers.MET;
@@ -23,18 +25,18 @@ namespace METCSV.WPF.ProductProvider
 
         private IProductReader GetProductReader()
         {
-            return new MetProductReader(_token);
+            return new MetProductReader(Token);
         }
 
         private IDownloader GetDownloader()
         {
-            if (App.Settings.Engine.OfflineMode)
+            if (offlineMode)
             {
                 return new MetOfflineDownloader();
             }
             else
             {
-                return new MetDownloader(App.Settings.MetDownlaoder, _token);
+                return new MetDownloader(settings);
             }
         }
     }
