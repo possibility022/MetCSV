ceb420a1774f4ac0bedb11374f70e8a3e6208b1b
Possibility <tomaszbakk@outlook.com>
2021-10-03 18:39:38 +0200

    Adding migration

--- MET.Data.Storage/Migrations/20211003163338_manufacturersProfits.Designer.cs
+++ MET.Data.Storage/Migrations/20211003163338_manufacturersProfits.Designer.cs
@@ -2,14 +2,16 @@
 using MET.Data.Storage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MET.Data.Storage.Migrations
 {
     [DbContext(typeof(StorageContext))]
-    partial class StorageContextModelSnapshot : ModelSnapshot
+    [Migration("20211003163338_manufacturersProfits")]
+    partial class manufacturersProfits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MET.Data.Storage/Migrations/20211003163338_manufacturersProfits.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MET.Data.Storage.Migrations
+{
+    public partial class manufacturersProfits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ManufacturerProfits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Manufacturer = table.Column<string>(type: "TEXT", nullable: true),
+                    Provider = table.Column<int>(type: "INTEGER", nullable: false),
+                    Profit = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManufacturerProfits", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManufacturerProfits");
+        }
+    }
+}
