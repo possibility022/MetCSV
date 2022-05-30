e7cc4ffc6d00b6dea43c405f1a57dbf2d69d6bba
Possibility <tomaszbakk@outlook.com>
2022-05-30 02:19:09 +0200

    Notifikacje

---
+++ METCSV.WPF/Helpers/Notification.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace METCSV.WPF.Helpers
+{
+    internal class Notification
+    {
+        public static void ShowNotification(bool success)
+        {
+            new ToastContentBuilder()
+                .AddText("MET CSV - Bzzzzz")
+                .AddText(success ? "Ukończono generowanie :)" : "Coś poczło nie tak :(")
+                .Show();
+        }
+    }
+}
